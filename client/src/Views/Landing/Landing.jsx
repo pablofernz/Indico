a63ab076e143ed74ea47f2dcff1ada06eb08c0f3
pabloferz <pablodanyfer@gmail.com>
2023-10-06 23:54:57 -0300

    modularizacion scroll top button

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import NavBar from "../../Components/Navbar/NavBar";
 import style from "./Landing.module.css";
-
+import ScrollTopButton from "../../Components/ScrollTopButton/ScrollTop";
 const Landing = () => {
   return (
     <div className={style.Landing}>
       <NavBar />
 
       <main>
-        <section className={style.HomeSection} id="Home">
+        <section className={style.HomeSection} id="Start">
           <div>Inicio</div>
         </section>
 
@@ -29,21 +29,7 @@ const Landing = () => {
         </section>
       </main>
 
-      <div className={style.button}>
-        <a href="#Home">
-          <svg>
-            <svg
-              className={style.arrow}
-              data-name="1-Arrow Up"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 32 32"
-              height={25}
-            >
-              <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z" />
-            </svg>
-          </svg>
-        </a>
-      </div>
+      <ScrollTopButton />
     </div>
   );
 };
